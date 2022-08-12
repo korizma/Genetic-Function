9a098b017fdbdd60c3fc0a6073738d81211b4e4b
Korizma <dulekorizma55@gmail.com>
2022-08-13 00:36:15 +0200

    dodato nasumicno generisanje funkcija preko dyke worda, stari nacin imao mnogo mana

--- pokusaj1/expression_random.py
+++ pokusaj1/expression_random.py
@@ -59,10 +59,71 @@ def generisi_random_funkciju_tree(dubina):
 
     return  glavna
 
-
+"""
 for x in range(1000000):
     m = generisi_random_funkciju_tree(7)
     print(m.ViewF())
     if (m.GetDepth() > 8):
         print("uzasssss")
         break
+   """
+
+#dyck word random tree
+
+#broj nodudova mora biti neparan
+def generisi_dyck_word(num_nodes):
+    l = int((num_nodes - 1) / 2)
+    rs = (num_nodes + 1) / 2
+
+    cn = math.factorial(num_nodes-1) / (math.factorial(l) * math.factorial(rs))
+
+    print("broj razlicitih struktura tree-a: " + str(cn))
+
+    prosla_pos = 2
+
+    yy_pozicije = [False for x in range(num_nodes)]
+    for i in range(l):
+        yy_pos = random.randint(prosla_pos+1, num_nodes - l + i)
+        prosla_pos = yy_pos
+        yy_pozicije[yy_pos] = True
+
+    word = ""
+    for pozicija in yy_pozicije:
+        word += "x"
+        if pozicija:
+            word += "yy"
+    return word
+
+#pretvaranje dyck reci u tree
+
+def napravi_terminali_node():
+    return generisi_random_funkciju()
+def napravi_neterminalni_node(f1, f2):
+    return klase.ComplexFunction(f1, f2, generisi_random_op())
+
+def napravi_stablo(word):
+    stek = []
+    duzina_reci = len(word)
+    i = 0
+
+    non_terminal = 1
+    terminal = 1
+
+    while i < duzina_reci:
+        if word[i+1] == 'y':
+            non_terminal = napravi_neterminalni_node(stek[len(stek)-1], stek[len(stek)-2])
+            stek = stek[:-2]
+            stek.append(non_terminal)
+            i += 3
+        else:
+            terminal = napravi_terminali_node()
+            stek.append(terminal)
+            i += 1
+
+    print(non_terminal.ViewF())
+
+
+
+word = generisi_dyck_word(9)
+print(word)
+napravi_stablo(word)
